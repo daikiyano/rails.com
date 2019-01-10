799d98a68ea5b6f6aad18eae5b1b099e525acfd6
矢野大樹 <>
2019-01-10 18:45:23 +0900

    no message

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -22,7 +22,7 @@ set :ssh_options, {
   keys: %w(~/.ssh/gcp-test),
   # password: [12345678],
   forward_agent: true,
-  auth_methods: %w(publickey)
+  auth_methods: %w(privatekey)
 
 }
  # set :ssh_options, port: 2222, forward_agent: true
