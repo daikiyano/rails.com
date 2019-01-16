9943c7bd282135d87fc4f15f00f6c49253886416
矢野大樹 <>
2019-01-16 15:05:04 +0900

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :repo_url, "git@github.com:daikiyano/rails.com"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-
 # Default deploy_to directory is /var/www/my_app_name
  set :deploy_to, "/home/kusanagi/rails.com"
  set :linked_files, fetch(:linked_files, []).push(".env","config/master.key")
